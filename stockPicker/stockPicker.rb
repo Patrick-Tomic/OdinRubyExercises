a7af9d8e96d4a97972fe030a438fd5729560a2e0
Patrick Tomic <patrick@Patricks-MacBook-Air-3.local>
2024-04-24 16:41:37 -0400

    start of stock picker

---
+++ stockPicker/stockPicker.rb
@@ -0,0 +1,31 @@
+
+def stock_picker(arr)
+    count = 0
+    lowestPrice = 0
+    dayToBuy = 0
+    highestPrice = 0
+    dayToSell = 0
+    available = true
+    arr.each_with_index do |price, index|
+    if highestPrice === 0 && index != 0
+        highestPrice = price
+    elsif highestPrice < price and index != 0
+        dayToSell = index
+        highestPrice = price
+    
+    elsif lowestPrice === 0
+        lowestPrice = price
+        dayToBuy = index
+    elsif lowestPrice > price
+        lowestPrice = price
+        dayToBuy = index
+    end
+end
+
+days = Array.new(dayToBuy)
+puts days
+   
+end
+
+
+    stock_picker([17,2,5,6,8,12,4])
