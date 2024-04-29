2438f7b1074e3dfed5780132f5201855a199fc56
Patrick-Tomic <pat12tomic@gmail.com>
2024-04-29 16:39:46 -0400

    skips over repeating number

--- bubbleSort/bubbleSort.rb
+++ bubbleSort/bubbleSort.rb
@@ -1,16 +1,31 @@
 def bubbleSort(arr)
     newArr = []
-    term = arr[0]
-   
-    arr.each_with_index do |value, index|
-    if(term > value)
-        newArr.push(value)
-    elsif term < value
-        newArr.push(term)
-        term = value
-    end
+    term = 0
+    bool = true
+    arr.each do |value|
+        if (value == arr[0])
+            term = arr[0]
+        elsif term === value
+            puts 'let it happen'
+            newArr.push(term)
+            term = value
+        elsif term > value
+            bool = false
+            newArr.push(value)
+        elsif(value >= term )
+            newArr.push(term)
+            term = value
+       
+        end
+end
+newArr.push(term)
+ 
+if(!bool)
+    bubbleSort(newArr)
+else
     puts newArr
 end
+
 end
 
 bubbleSort([4,3,78,2,0,2])
